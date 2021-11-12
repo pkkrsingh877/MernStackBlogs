9994ccd3c2e327590617299f55a6d617a422df4b
Prabhat Kumar <pkkrsingh877@gmail.com>
2021-11-13 05:10:48 +0530

    logic for signup error handling done

--- functions/signupErrorHandler.js
+++ functions/signupErrorHandler.js
@@ -3,10 +3,14 @@ const signupErrorHandler = (err) => {
         username: '',
         password: '',
         email: ''
-    }
-    
+    }    
     if(err.code === 11000){
-        error.email = "This email is already registered!"
+        if(err.keyValue.username){
+            error.username = "This username is already registered!"
+        }
+        if(err.keyValue.email){
+            error.email = "This email is already registered!"
+        }
         console.log(error);
         return error;
     }
