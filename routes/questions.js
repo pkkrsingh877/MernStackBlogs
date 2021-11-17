4ef14479bc3f27f1c083fbb75e2d69a560dc22d0
Prabhat Kumar <pkkrsingh877@gmail.com>
2021-11-17 19:02:01 +0530

    basic questions route done

--- routes/questions.js
+++ routes/questions.js
@@ -1,19 +1,32 @@
 const express = require('express');
 const Question = require('../models/questions');
+const prepareSomeTags = require('../functions/prepareSomeTags')
 const router = express.Router();
-
-router.post('/', async (req, res) => {
-    const { title, description, tags, password } = req.body;
-    res.status(200).send("It's working");
-});
 
 router.get('/new', async (req, res) => {
     res.render('questions/new');
 });
 
+router.post('/', async (req, res) => {
+    const { title, description, tags } = req.body;
+    let prepareTags = prepareSomeTags(tags);
+    const question = await Question.create({
+        title: title, 
+        description: description,
+        tags: prepareTags
+    });
+    res.status(200).redirect("/questions");
+});
+
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    const question = await Question.findById(id);
+    res.render('questions/show', { question });
+});
+
 router.get('/', async (req, res) => {
     const questions = await Question.find({});
-    res.render('questions/index', {});
+    res.render('questions/index', { questions });
 });
 
 module.exports = router;
