ba922f0f67d860386ac7eb1446f5aa404a761b9a
Prabhat Kumar <pkkrsingh877@gmail.com>
2021-11-21 05:59:08 +0530

    update user feature done

--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,30 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/users");
+const checkUser = require('../middlewares/checkUserMiddleware');
 
-router.get('/update', async (req, res) => {
+router.post('/update', checkUser, async (req, res) => {
+    try {
+        if(res.locals.user.id){
+            const { name, username, email, img, bio } = req.body;
+            const user = await User.findByIdAndUpdate(res.locals.user.id, { name, username, email, img, bio }, { new: true});
+            console.log(user);
+            res.status(200).json({ user: user._id });
+        }else{
+            res.status(200).redirect('/auth/login');
+        }
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+router.get('/update', checkUser, async (req, res) => {
     res.render('user/updateProfile.ejs');
 });
 
-router.get('/:id', async (req, res) => {
-    const { id } = req.params;
-    console.log(id)
+router.get('/profile', checkUser, async (req, res) => {
     try{
-        const data = await User.findById(id);
+        const data = await User.findById(res.locals.user.id);
         res.status(200).render('user/profile', { data })
     }catch(err){
         res.status(404).render('error');
