71dc57946185b00e36c69946e5dc2de1e0d4c468
Prabhat Kumar <pkkrsingh877@gmail.com>
2021-11-24 14:58:24 +0530

    added pagination on saved articles

--- routes/user.js
+++ routes/user.js
@@ -4,8 +4,50 @@ const User = require("../models/users");
 const checkUser = require('../middlewares/checkUserMiddleware');
 
 router.get('/savedarticles', checkUser, async (req, res) => {
-    const savedArticles = res.locals.user.saved;
-    res.status(200).render('user/savedArticles', { savedArticles });
+    let { page, skip, limit } = req.query;
+    limit = parseInt(limit);
+    page = parseInt(page);
+    skip = parseInt(skip);
+    if (!page || page < 0) {
+        page = 1;
+    }
+    if (!skip || skip < 0) {
+        skip = 4;
+    }
+    if (!limit || limit < 0) {
+        limit = 4;
+    }
+    const currentPage = parseInt(page);
+    let temp = currentPage;
+    if (temp != 1) {
+        temp--;
+    }
+    try {
+        const count = res.locals.user.saved.length;
+
+        const totalPages = Math.ceil(count / limit);
+        if (page > totalPages) {
+            temp = totalPages - 1;
+            page = totalPages;
+        }
+
+        let savedArticles = res.locals.user.saved;
+        console.log("full", savedArticles);
+        const skipAndLimit = (page, skip) => {
+            savedArticles = savedArticles.slice((page - 1)*skip, ((page - 1)*skip)+4);
+            console.log(page, skip)
+            console.log("skip", savedArticles);
+            savedArticles = savedArticles.slice(0, 4);
+            console.log("limit", savedArticles);
+        }    
+
+        skipAndLimit(page, skip);
+
+        res.status(200).render("user/savedArticles", { savedArticles , totalPages, currentPage, temp });
+    } catch (err) {
+        console.log(err);
+        res.status(404).render("error");
+    }
 });
 
 router.post('/save', checkUser, async (req, res) => {
