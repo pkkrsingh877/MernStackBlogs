4fa0a403a9527c54303e9c72eba986c80a73affc
Prabhat Kumar <pkkrsingh877@gmail.com>
2021-12-06 13:36:09 +0530

    editor feature update
    editors can now list all articles that they have written and edit, view or delete it.

--- routes/editor.js
+++ routes/editor.js
@@ -1,8 +1,61 @@
 const express = require("express");
 const router = express.Router();
 const Article = require("../models/articles");
+const User = require("../models/users");
 const prepareSomeTags = require('../functions/prepareSomeTags');
 const readMinutes = require("../functions/readMinutes");
+
+router.delete('/articles/delete/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        await Article.findByIdAndDelete(id);
+        res.status(200).redirect('/editor/articles');
+    } catch (err) {
+        console.log(err);
+        res.status(400).render('error');
+    }
+});
+
+router.patch('/articles/edit/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { title, description, tags } = req.body;
+        const article = await Article.findByIdAndUpdate(id, {
+            title: title,
+            description: description.trim(),
+            tags: prepareSomeTags(tags)
+        },{
+            new: true
+        });
+        console.log(article);
+        res.status(200).redirect('/editor/articles');
+    } catch (err) {
+        console.log(err);
+        res.status(400).render("error");
+    }
+});
+
+router.get('/articles/edit/:id', async (req, res) => {
+    try {        
+        const { id } = req.params;
+        const article = await Article.findById(id);
+        res.render('editor/editArticle', { article });
+    } catch (err) {
+        console.log(err);
+        res.status(400).render('error');
+    }
+});
+
+router.get('/articles', async (req, res) => {
+    try {
+        const user = await User.findById(res.locals.user._id).populate('articles');
+        const articles = user.articles;
+        res.status(200).render('editor/articles', { articles });
+    }catch(err){
+        console.log(err);
+        res.status(400).render('error');
+    }
+});
 
 router.post('/', async (req, res) => {
     try {
